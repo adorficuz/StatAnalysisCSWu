e81d78719f51df0854623e6870005ee7c9ab5682
EQUIPO <adolfovr3@gmail.com>
2023-10-15 19:58:05 +0200

    a

--- 3DPLOT.py
+++ 3DPLOT.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import SymLogNorm, Normalize, LogNorm, FuncNorm
+from matplotlib.ticker import MaxNLocator
 
 d3d = dict()
 for i in m.d.keys():
@@ -24,20 +26,23 @@ lx = np.array(list(d3d.keys()))
 ly = np.array(list((d3d[1940].keys())))
 x,y = np.meshgrid(lx,ly)
 
-x = x.flatten()
-y = y.flatten()
-
 dz = list()
 
-for i,xi in enumerate(x):
-    dz.append((d3d[xi])[y[i]])
+for i,xi in enumerate(x.flatten()):
+    dz.append((d3d[xi])[(y.flatten())[i]])
 
 dz = np.array(dz)
-
-
 z = dz.reshape(ly.shape[0],lx.shape[0])
 
-plt.imshow( z , cmap = 'jet' , interpolation = 'gaussian' , origin='lower',
-           aspect='equal',  extent = [min(x), max(x), min(y), max(y)] )
-plt.colorbar()
+def _forward(x):
+    return (x-20)**3 - (z.min()-20)**3
+
+def _inverse(x):
+    return (x+(z.min()-20)**3)**(1/3) + 20
+
+
+fig, ax = plt.subplots()
+norm = FuncNorm((_forward, _inverse), vmin=z.min(), vmax=z.max())
+pcm = ax.pcolormesh(x, y, z, norm=norm, cmap='jet', shading='auto')
+ax.set_title('FuncNorm(x)')
 plt.show()
